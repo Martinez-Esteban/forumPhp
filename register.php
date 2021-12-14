b7b7b894bd319b2db8fae4652715cccfc03f4e0c
EMARTINEZ <esteban.martinez@ynov.com>
2021-12-14 12:17:31 +0100

    push register files

---
+++ register.php
@@ -0,0 +1,60 @@
+<?php
+
+$dsn = "mysql:host=localhost:3306;dbname=forum";
+$username = "root";
+$password = "";
+
+try{
+    $pdo = new PDO($dsn, $username, $password);
+} catch (PDOException $e){
+    echo $e->getMessage();
+    die();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <link href='./register.css' rel='style'>
+    </head>
+    <body>
+        <?php
+
+        function verifyPassword($pdo) {
+            if($_POST['password'] === $_POST['passwordVerified']) {
+                addUser($pdo);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function addUser($pdo) {
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $mail = $_POST['mail'];
+
+            $query = 'INSERT INTO user(username, password, email) VALUES(:username, :password, :mail)';
+
+            $datas = [
+                'username' => $username,
+                'password' => $password,
+                'mail' => $mail,
+            ];
+
+            $results = $pdo->prepare($query);
+            $results->execute($datas);
+        }
+
+        ?>
+        <form class="registerForm" method="POST" onsubmit="<?=verifyPassword($pdo)?>">
+            <input type="text" name="username" required><br>
+            <input type="password" name="password" required><br>
+            <input type="password" name="passwordVerified" required><br>
+            <input type="email" name="mail" required><br>
+            <input type="submit" name="button" value="Créer un compte"><br>
+        </form>
+    </body>
+</html>
