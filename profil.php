cc5e2ca306c29976cd40f66fdb0cf03f8db70ed3
Esteban Martinez <esteban.martinez@ynov.com>
2021-12-20 15:26:55 +0100

    fix profil and add modify password function

--- profil.php
+++ profil.php
@@ -9,24 +9,29 @@ if ($_SESSION["newsession"]) {
 ?>
 
 <?php
-  // On récupère les informations de l'utilisateur connecté
-  $afficher_profil = $mysqli->query("SELECT * FROM user WHERE username = '" . $_SESSION["newsession"] . "'");
-  $a = $afficher_profil->fetch();
 
-  function verifyPassword($mysqli) {
-	global $a;
-	global $mysqli;
-	$pwdtest = password_hash($_POST['oldpwd'], PASSWORD_BCRYPT);
-	if($pwdtest === $a['password'] && $_POST['newpwd'] === $_POST['confirmpwd']) {
-		$query = "UPDATE user SET password = '" . password_hash($_POST['newpwd'], PASSWORD_BCRYPT) . "' WHERE username = '" . $_SESSION['newsession'] . "'";
-		$update = $mysqli->prepare($query);
-		$update->execute();
-	} else {
-		echo "bah non";
+	// On récupère les informations de l'utilisateur connecté
+	$afficher_profil = $mysqli->query("SELECT * FROM user WHERE username = '" . $_SESSION["newsession"] . "'");
+	$a = $afficher_profil->fetch();
+
+	$error = "";
+
+	if(isset($_POST['oui'])) {
+		global $a;
+		global $mysqli;
+		$pwdtest = password_hash($_POST['oldpwd'], PASSWORD_BCRYPT);
+		if(password_verify($_POST['oldpwd'], $a['password']) && $_POST['newpwd'] === $_POST['confirmpwd']) {
+			$query = "UPDATE user SET password = '" . password_hash($_POST['newpwd'], PASSWORD_BCRYPT) . "' WHERE username = '" . $_SESSION['newsession'] . "'";
+			$update = $mysqli->prepare($query);
+			$update->execute();
+			$error = "Mot de passe modifié avec succès !";
+		} elseif (!password_verify($_POST['oldpwd'], $a['password'])) {
+			$error = "Mauvais mot de passe.";
+		} else {
+			$error = "Les mots de passes ne correspondent pas.";
+		}
 	}
-}
 ?>
-
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
@@ -36,23 +41,20 @@ if ($_SESSION["newsession"]) {
 	<head>
 	<body>
 		<a href="./index.php">retour</a>
-		<h2>Voici le profil de <?= $_SESSION["newsession"];?></h2>
+		<a href="./deconnexion.php">Deconnexion</a>
+		<h2>Votre profil :</h2>
 		<div>Quelques informations sur vous : </div>
     	<ul>
-            <li>Votre id est : <?php echo $a['id']?></li>
-            <li>Votre mail est : <?php echo $a['email'] ?></li>
+			<li>Nom d'utilisateur : <b><?= $_SESSION["newsession"];?></b></li>
+            <li>Votre mail est : <b><?php echo $a['email'] ?></b></li>
 		</ul>
-		<form method="POST" onsubmit="<?=verifyPassword($mysqli)?>">
+		<h3>Changer de mot de passe :</h3>
+		<form method="POST">
 			<input type="password" name="oldpwd" placeholder="Ancien mot de passe" required /><br>
 			<input type="password" name="newpwd" placeholder="Nouveau mot de passe" required /><br>
-			<input type="password" name="confirmpdw" placeholder="Confirmer nouveau mot de passe" required /><br>
-			<input type="submit" value="Valider">
+			<input type="password" name="confirmpwd" placeholder="Confirmer nouveau mot de passe" required /><br>
+			<input type="submit" name="oui" value="Valider">
 		</form>
+		<?= $error ?>
 	</body>
 </html>
-
-<?php
-
-
-
-?>
