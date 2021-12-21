12b953372d157c4b3f0b0a6eb66564438e4740d2
BGELINEAU <benjamin.gelineau@ynov.com>
2021-12-21 16:34:12 +0100

    serarch

--- search.php
+++ search.php
@@ -3,12 +3,12 @@
 $mysqli = new PDO("mysql:host=127.0.0.1;dbname=forum;charset=utf8", "root", "");
 
 if(isset($_GET['search'])){
-    echo("commande executer!");
+    echo("Voici les résultats de votre recherche!");
     $delete_user = $mysqli->prepare("SELECT * FROM articles WHERE title LIKE '%".$_GET['search']."%'");
     $delete_user ->execute();
     echo "<ul>";
         while ($row = $delete_user->fetch()) {
-            echo "<li><a href=./details?articleId=".$row['articleId'].">".$row['title']."<br>".$row['description']."</a></li>";  
+            echo "<li><a href=./details.php?articleId=".$row['articleId'].">".$row['title']."<br>".$row['description']."</a></li>";  
         }
     echo "</ul>";    
     
