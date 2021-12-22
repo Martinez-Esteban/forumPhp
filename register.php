1e960dbefdb6604e41543513595ce1fc9e43eacf
Esteban Martinez <esteban.martinez@ynov.com>
2021-12-22 13:42:10 +0100

    add link to pp

--- register.php
+++ register.php
@@ -35,12 +35,13 @@ try{
             $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
             $mail = $_POST['mail'];
 
-            $query = 'INSERT INTO user(username, password, email, creationDate) VALUES(:username, :password, :mail, NOW())';
+            $query = 'INSERT INTO user(username, password, email, creationDate, pp) VALUES(:username, :password, :mail, NOW(), :pp)';
 
             $datas = [
                 'username' => $username,
                 'password' => $password,
                 'mail' => $mail,
+                'pp' => "https://www.cournondanseattitude.fr/wp-content/uploads/2019/07/blank-profile-picture-973460_640.png",
             ];
 
             $results = $pdo->prepare($query);
@@ -48,10 +49,11 @@ try{
         }
         ?>
         <form class="registerForm" method="POST" onsubmit="<?=verifyPassword($pdo)?>">
-            <input type="text" name="username" required placeholder="Nom d'utilisateur"><br>
-            <input type="password" name="password" required placeholder="Mot de passe"><br>
-            <input type="password" name="passwordVerified" required placeholder="Confirmation du mot de passe"><br>
-            <input type="email" name="mail" required placeholder="E-mail"><br>
+            <input type="text" name="username" required placeholder="Nom d'utilisateur*"><br>
+            <input type="password" name="password" required placeholder="Mot de passe*"><br>
+            <input type="password" name="passwordVerified" required placeholder="Confirmation du mdp*"><br>
+            <input type="email" name="mail" required placeholder="E-mail*"><br>
+            <input type="text" name="profilPicture" placeholder="lien vers la photo de profil"><br>
             <input type="submit" name="button" value="Créer un compte"><br>
         </form>
         <a href="./login.php">Login</a>
